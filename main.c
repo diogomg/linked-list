2b97b9104b1874c2ad37d36d78edf04933338a58
Diogo Gonçalves <diogomachadogoncalves@gmail.com>
2015-01-30 05:17:40 -0200

    [src] add a dummy main.c file

--- main.c
+++ main.c
@@ -3,6 +3,13 @@
 
 int main(){
 
-    
+    listNode *node = NULL;
+
+    insertBefore(&node, 1);
+    insertBefore(&node, 2);
+    printInOrder(node);
+    printf("%p\n", member(&node, 1));
+    printf("%p\n", member(&node, 2));
+
     return 0;
 }
